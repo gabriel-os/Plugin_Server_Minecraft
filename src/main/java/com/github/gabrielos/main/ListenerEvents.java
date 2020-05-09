601559f7871ec53c408338f6a1e15081ee85a4bc
Gabriel Santos <o.gabriel@aluno.ufabc.edu.br>
2020-05-09 14:53:10 -0300

    Removendo sysout

--- src/main/java/com/github/gabrielos/main/ListenerEvents.java
+++ src/main/java/com/github/gabrielos/main/ListenerEvents.java
@@ -9,15 +9,15 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 public class ListenerEvents implements Listener{
 	@EventHandler
 	public void onDamageEvent(EntityDamageByEntityEvent e) {
-		System.out.println("1");
+		
 		if(e.getEntity() instanceof LivingEntity) {
-			System.out.println("2");
+			
 			LivingEntity entidade = ((LivingEntity) e.getEntity());
 			
 			if(e.getDamager()!= null) {
-				System.out.println("3");
+				
 				if (e.getDamager() instanceof Player) {
-					System.out.println("4");
+					
 					Player player = (Player)e.getDamager();
 					
 					player.sendMessage("Pimba: " + (entidade.getHealth()-e.getDamage()));					
